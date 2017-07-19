8d809578e24fe075e8bec6e42b9989883884485b
Wei Wang <wangw@codeaurora.org>
2017-07-19 13:27:08 -0700

    soong: add new linker property qti_whole_static_libs
    add the following linker property to cc_library to statically
    link libraries only for qti targets
    
    qti_whole_static_libs: [
      "mylib"
    ]
    
    Change-Id: Ib40faf09afac6a7efe5273b13e5bbace519f21f8
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- android/variable.go
+++ android/variable.go
@@ -183,6 +183,8 @@ type productVariables struct {
 	Override_rs_driver *string `json:",omitempty"`
 
 	DeviceKernelHeaders []string `json:",omitempty"`
+
+	BoardUsesQTIHardware *bool `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
@@ -214,6 +216,7 @@ func (v *productVariables) SetDefaultConfig() {
 		DeviceSecondaryAbi:         &[]string{"armeabi-v7a"},
 		Malloc_not_svelte:          boolPtr(false),
 		Safestack:                  boolPtr(false),
+		BoardUsesQTIHardware:      boolPtr(false),
 	}
 
 	if runtime.GOOS == "linux" {
