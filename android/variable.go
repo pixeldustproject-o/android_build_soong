05c922692e6f07c2d1b2b70f40667af03a0ae39d
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:33:26 +0200

    variables: Add uses_media_extensions product variable
    Change-Id: I257dfe51be7e771611dd57ae57f073ab6fe6d392
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- android/variable.go
+++ android/variable.go
@@ -71,6 +71,10 @@ type variableProperties struct {
 			Cflags []string
 		}
 
+		Uses_media_extensions struct {
+			Cflags []string
+		}
+
 		// treble is true when a build is a Treble compliant device.  This is automatically set when
 		// a build is shipped with Android O, but can be overriden.  This controls such things as
 		// the sepolicy split and enabling the Treble linker namespaces.
@@ -146,6 +150,7 @@ type productVariables struct {
 	Pdk                        *bool `json:",omitempty"`
 	Libart_img_base            *string `json:",omitempty"`
 	Has_legacy_camera_hal1     *bool `json:",omitempty"`
+	Uses_media_extensions      *bool `json:",omitempty"`
 
 	IntegerOverflowExcludePaths *[]string `json:",omitempty"`
 
